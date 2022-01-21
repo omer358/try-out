c9351a43168c641041fea3addaea50071f11bfaa
omer358 <omermaki358@gmail.com>
2022-01-21 09:40:29 +0200

    Edit youtube downloader

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -5,13 +5,12 @@ Note: By default, the download path will be in the project directory.
     you can leave it that way or you can change it by using the os module
     and save the video wherever you want to.
 """
-from pytube import YouTube
+
 import os
+from pytube import YouTube
 
-# change the path to where you want to save the video
+# change the dir to where you want to save the video
 os.chdir("/home/omer358/Downloads/youtube_videos")
-
-# go to the youtube video page and copy the URL
 link = input("Enter your Youtube URL: ")
 yt = YouTube(link)
 queries = yt.streams
@@ -20,10 +19,10 @@ for opt in download_opts:
     print(opt)
 
 print("Enter the desired option: ")
-choice = int(input("enter the option: "))
+choice = int(input("Your option is: "))
 dn_video = queries[choice]
 video_size = dn_video.filesize / (1024 * 1024)
-print("the total video size is:{:.2f}MB".format(video_size))
-print("Downloading...")
+print("The total video size is: {:.2f}MB".format(video_size))
+print("Downloading..")
 dn_video.download()
-print("Download has been successfully completed in: {}".format(os.getcwd()))
+print("Download has been successfully completed in : {}".format(os.getcwd()))
